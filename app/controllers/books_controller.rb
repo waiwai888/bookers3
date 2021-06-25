f487b3c03902f492978b5051509abe0b267ca8d1
waiwai888 <yuisato9688@gmail.com>
2021-06-25 22:27:26 +0000

    [bookers3]booksアクション設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,34 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
+  def index
+    @books = Book.all
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def post_image_params
+    params.require(:book_params).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :users
+  resources :users, only: [:new, :create, :index, :show, :destroy]
   resources :books
   get 'homes/about' => 'homes#index'
 end
