15e3af74a5048ec8a286ee2c92ed4383ce0bb064
waiwai888 <yuisato9688@gmail.com>
2021-06-28 01:18:47 +0000

    [bookers3]ヘッダーリンク完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,10 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update
     redirect_to user_path
-
+  end
 end
